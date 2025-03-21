4768234e369d0f34ac0c6bc09159f1959d9c1e96
vikas <>
2025-03-21 13:36:33 +0530

    race.js

--- race.js
+++ race.js
@@ -8,9 +8,9 @@ Promise.race([p1, p2, p3]).then(console.log)
 .catch((err)=>{
      console.log(err);
  }); 
- // rece function of promise take an array of return  and retunr only those promise jo sbse pahle resolve ya reject huya ho upper wale code pe hame burger millega out put pe....
+ // rece function of promise take an array of promises  and return only those promise jo sbse pahle resolve ya reject huya ho upper wale code pe hame burger millega out put pe....
 
- // niche wale code pe sbse pahle promise reject ho raha hai to  hame catch block pe hotdogUnavilable millega
+ // niche wale code pe sbse pahle promise reject ho raha hai to  hame catch block pe hotdog Unavilable millega
  let p4 = new Promise((resolved,rejeced )=> setTimeout(() => resolved("🍕 Pizza"), 3000));
 let p5 = new Promise((resolved, rejected)=> setTimeout(() => resolved("🍔 Burger"), 2000));
 let p6 = new Promise((resolved ,rejected)=> setTimeout(() => rejected("hotdog unvaliable"), 1000));
